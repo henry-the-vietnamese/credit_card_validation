dcc4484fb4e1135d531a0dfa8180e68a31eb076a
Tan Duc Mai <tan.duc.work@gmail.com>
2022-06-11 00:23:11 +0930

    Implement DbC approach

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-# -----------------------------------------------------------------------------
-# |
-# | File:         main.py
-# | Author:       Tan Duc Mai
-# | Email:        tan.duc.work@gmail.com
-# | Date:         17-Dec-2021
-# | Description:
-# |   I hereby declare that I completed this work without any improper help
-# |   from a third party and without using any aids other than those cited.
-# |
-# -----------------------------------------------------------------------------
+# =============================================================================
+#
+#        FILE:  main.py
+#      AUTHOR:  Tan Duc Mai
+#       EMAIL:  tan.duc.work@gmail.com
+#        DATE:  17-Dec-2021
+# DESCRIPTION:  Use Hans Peter Luhn's algorithm to validate credit card number
+#   I hereby declare that I completed this work without any improper help
+#   from a third party and without using any aids other than those cited.
+#
+# =============================================================================
+
+# ------------------------------- Module Import -------------------------------
+import icontract
 
 
 # ---------------------------- Function Definitions ---------------------------
+@icontract.require(lambda total: isinstance(total, int))
+@icontract.ensure(lambda result: isinstance(result, bool))
 def check_moduo_10(total):
     """
       Check if total moduo 10 is equal to 10.
@@ -27,6 +33,8 @@ def check_moduo_10(total):
         return False
 
 
+@icontract.require(lambda number: number.isnumeric())
+@icontract.ensure(lambda result: isinstance(result, int))
 def add_digits(number):
     """Return the sum of doubled digits with the undoubled digits."""
     # Initialise an accumulator to 0.
@@ -46,6 +54,8 @@ def add_digits(number):
     return sum_digits
 
 
+@icontract.require(lambda card_number: isinstance(card_number, str))
+@icontract.ensure(lambda result: isinstance(result, str))
 def reversed_number(card_number):
     """From the card number, reverse it so it goes from right to left."""
     new_number = ''
@@ -62,7 +72,7 @@ def main():
     card_number = input('Please enter card number: ')
 
     # Add digits up and moduo 10, display the result.
-    print(f'{repr(card_number)} is', end= ' ')
+    print(f'{repr(card_number)} is', end=' ')
     if check_moduo_10(add_digits((reversed_number(card_number)))):
         print('valid')
     else:
@@ -71,4 +81,7 @@ def main():
 
 # --------------------------- Call the Main Function --------------------------
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except icontract.errors.ViolationError as e:
+        print(e)
