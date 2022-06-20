f599286f8dd9f40c2bb49bb9d1e80917a2acfa6d
Tan Duc Mai <tan.duc.work@gmail.com>
2022-06-21 01:08:14 +0930

    Format code with black

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ import icontract
 @icontract.ensure(lambda result: isinstance(result, bool))
 def check_moduo_10(total):
     """
-      Check if total moduo 10 is equal to 10.
-      Return True if if it is; False otherwise.
+    Check if total moduo 10 is equal to 10.
+    Return True if if it is; False otherwise.
     """
     # IF the total modulo 10 is equal to 0: number is valid.
     if total % 10 == 0:
-       return True
+        return True
     # ELSE: number is invalid.
     else:
         return False
@@ -60,7 +60,7 @@ def reversed_number(card_number):
     """From the card number, reverse it so it goes from right to left."""
     new_number = ''
 
-    for digit in range(len(card_number)-1, -1, -1):
+    for digit in range(len(card_number) - 1, -1, -1):
         new_number += card_number[digit]
 
     return new_number
